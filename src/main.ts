c5da106bc1114eae6385a5a9a1a13776120af7d4
TylerMeek <1755276345@qq.com>
2023-06-12 13:14:18 +0800

    fix: 依赖修复

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import FishDesign from "@tyler_the_duck/fish-design";
-import "@tyler_the_duck/fish-design/style.css";
+import FishDesign from "fish-design-vite";
+import "fish-design-vite/style.css";
 
 createApp(App)
     .use(FishDesign).mount('#app')
